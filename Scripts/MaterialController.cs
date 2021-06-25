fe81cc2c7f5ca6affb28ff6711431286b0eb89bf
Vale <58179315+DelosIX@users.noreply.github.com>
2021-06-25 06:23:46 +0100

    Initial Upload

---
+++ Scripts/MaterialController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace StubbedConverter
+{
+    public static class MaterialController
+    {
+        // An tool to add KomradeSpectre's Material Controller Component to any object's renderers.
+        // Make use of RuntimeInspector to inspect a gameobject in question after adding the compoennt.
+        // inspecting the details of the Material Controller will allow control of any material in game while playing.
+
+        /// <summary>
+        /// Adds a Material Controller (by KomradeSpectre) to all inObject's Renderers. Enables control of materials in game through RuntimeInspector.
+        /// </summary>
+        public static void AddMaterialController(GameObject inObject, bool debug)
+        {
+            List<Renderer> objectRenderers = new List<Renderer>();
+            objectRenderers.AddRange(inObject.GetComponents<Renderer>());
+            objectRenderers.AddRange(inObject.GetComponentsInChildren<Renderer>());
+            if (debug) Debug.Log("RuntimeMaterialController: Found " + objectRenderers.Count + " renderers within GameObject: " + inObject);
+            foreach (Renderer r in objectRenderers.ToArray())
+            {
+                AddMaterialControllerRenderer(r, debug);
+            }
+        }
+        public static void AddMaterialController(GameObject inObject)
+        {
+            AddMaterialController(inObject, false);
+        }
+        private static void AddMaterialControllerRenderer(Renderer inRenderer, bool debug)
+        {
+            var finder = inRenderer.gameObject.AddComponent<StubbedConverter.Utils.MaterialControllerComponents.HGControllerFinder>();
+            finder.MeshRenderer = inRenderer;
+            if (debug) Debug.Log("RuntimeMaterialController: Added RuntimeMaterialController to GameObject: " + inRenderer.gameObject + " for Renderer:" + inRenderer);
+        }
+    }
+}
---
+++ ShaderConverterPlugin.cs
@@ -0,0 +1,90 @@
+﻿using BepInEx;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StubbedConverter
+{
+    [BepInPlugin(MODUID, MODNAME, MODVERSION)]
+    internal class ShaderConverterPlugin : BaseUnityPlugin
+    {
+        public const string MODUID = "com.valex.ShaderConverter";
+        public const string MODNAME = "ShaderConverter";
+        public const string MODVERSION = "0.0.3";
+
+        public static ShaderConverterPlugin instance;
+
+        internal static Material cloudMat;
+
+        // This Dictionary ties every stubbed version of a shader to the one that vanilla RoR2 uses. Yes, even the shaders that don't necessarily need this dictionary or stubbed conversion.
+        // I've already written them all down. If it works when you use the stubbed version, then it works.
+        internal readonly static Dictionary<string, string> stubbedShaderLookup = new Dictionary<string, string>()
+        {
+            //Shader internal name (Input shader)                               Path name (Output shader)
+            {"stubbed_Hidden/FastBlur Proxy",                                   "shaders/mobileblur" },
+            {"stubbed_TextMeshPro/Bitmap",                                      "shaders/tmp_bitmap"},
+            {"stubbed_TextMeshPro/Bitmap Custom Atlas",                         "shaders/tmp_bitmap-custom-atlas"},
+            {"stubbed_TextMeshPro/Mobile/Bitmap",                               "shaders/tmp_bitmap-mobile" },
+            {"stubbed_TextMeshPro/Distance Field",                              "shaders/tmp_sdf"},
+            {"stubbed_TextMeshPro/Distance Field Overlay",                      "shaders/tmp_sdf overlay"},
+            {"stubbed_TextMeshPro/Mobile/Distance Field",                       "shaders/tmp_sdf-mobile"},
+            {"stubbed_TextMeshPro/Mobile/Distance Field - Masking",             "shaders/tmp_sdf-mobile masking"},
+            {"stubbed_TextMeshPro/Mobile/Distance Field Overlay",               "shaders/tmp_sdf-mobile overlay"},
+            {"stubbed_TextMeshPro/Distance Field (Surface)",                    "shaders/tmp_sdf-surface"},
+            {"stubbed_TextMeshPro/Mobile/Distance Field (Surface)",             "shaders/tmp_sdf-surface-mobile"},
+            {"stubbed_TextMeshPro/Sprite",                                      "shaders/tmp_sprite"},
+            {"stubbed_Hopoo Games/Deferred/Snow Topped Proxy",                  "shaders/deferred/hgsnowtopped"},
+            {"stubbed_Hopoo Games/Deferred/Standard Proxy",                     "shaders/deferred/hgstandard"},
+            {"stubbed_Hopoo Games/Deferred/Triplanar Terrain Blend Proxy",      "shaders/deferred/hgtriplanarterrainblend"},
+            {"stubbed_Hopoo Games/Environment/Distant Water Proxy",             "shaders/environment/hgdistantwater"},
+            {"stubbed_Hopoo Games/Environment/Waving Grass Proxy",              "shaders/environment/hggrass"},
+            {"stubbed_Hopoo Games/Environment/Waterfall Proxy",                 "shaders/environment/hgwaterfall"},
+            {"stubbed_Hopoo Games/FX/Cloud Remap Proxy",                        "shaders/fx/hgcloudremap"},
+            {"stubbed_Hopoo Games/FX/Damage Number Proxy",                      "shaders/fx/hgdamagenumber"},
+            {"stubbed_Hopoo Games/FX/Distortion Proxy",                         "shaders/fx/hgdistortion"},
+            {"stubbed_Hopoo Games/FX/Forward Planet Proxy",                     "shaders/fx/hgforwardplanet"},
+            {"stubbed_Hopoo Games/FX/Cloud Intersection Remap Proxy",           "shaders/fx/hgintersectioncloudremap"},
+            {"stubbed_Hopoo Games/FX/Opaque Cloud Remap Proxy",                 "shaders/fx/hgopaquecloudremap"},
+            {"stubbed_Hopoo Games/FX/Solid Parallax Proxy",                     "shaders/fx/hgsolidparallax"},
+            {"stubbed_Hopoo Games/FX/Vertex Colors Only Proxy",                 "shaders/fx/hgvertexonly"},
+            {"stubbed_Hopoo Games/Internal/Outline Highlight Proxy",            "shaders/postprocess/hgoutlinehighlight"},
+            {"stubbed_Hopoo Games/Post Process/Scope Distortion Proxy",         "shaders/postprocess/hgscopeshader"},
+            {"stubbed_Hopoo Games/Post Process/Screen Damage Proxy",            "shaders/postprocess/hgscreendamage"},
+            {"stubbed_Hopoo Games/Internal/SobelBuffer Proxy",                  "shaders/postprocess/hgsobelbuffer"},
+            {"stubbed_Hopoo Games/UI/Animate Alpha Proxy",                      "shaders/ui/hguianimatealpha"},
+            {"stubbed_Hopoo Games/UI/UI Bar Remap Proxy",                       "shaders/ui/hguibarremap"},
+            {"stubbed_Hopoo Games/UI/Masked UI Blur Proxy",                     "shaders/ui/hguiblur"},
+            {"stubbed_Hopoo Games/UI/Custom Blend Proxy",                       "shaders/ui/hguicustomblend"},
+            {"stubbed_Hopoo Games/UI/Debug Ignore Z Proxy",                     "shaders/ui/hguiignorez"},
+            {"stubbed_Hopoo Games/UI/Default Overbrighten Proxy",               "shaders/ui/hguioverbrighten"},
+            {"StubbedShader/fx/hgcloudremap",                                   "shaders/fx/hgcloudremap"}
+        };
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void Start()
+        {
+            cloudMat = Resources.Load<GameObject>("Prefabs/Effects/OrbEffects/LightningStrikeOrbEffect").transform.Find("Ring").GetComponent<ParticleSystemRenderer>().material;
+            Debug.Log("ShaderConverter: Gathered Cloud Remap material: " + cloudMat + "!");
+            Debug.Log("ShaderConverter: Processing all Asset Bundles in queue...");
+            this.ProcessAssetBundles();
+            Debug.Log("ShaderConverter: Finished processing Asset Bundle materials!");
+        }
+
+        private void ProcessAssetBundles()
+        {
+            int i = 0;
+            foreach(AssetBundle bundle in ShaderConvert.assetBundles)
+            {
+                Debug.Log("ShaderConverter: Processing Asset Bundle: " + bundle + ". Debug: " + ShaderConvert.debugList[i]);
+                //ShaderConvert.ProcessAssetBundleShaders(bundle, ShaderConvert.debugList[i]);
+                ShaderConvert.ProcessAssetBundleShadersCloudFix(bundle, ShaderConvert.debugList[i]);
+                i++;
+                
+            }
+        }
+    }
+}
